adf8ae9b60b4bd47cb0efcb195d7ed130f12ee46
Evolved Bag <undercloudstudio@gmail.com>
2019-02-10 21:44:12 -0700

    Filled out stat scripts
    filled TowerStat and EnemyStat with necessary variables, also typed up a much needed table of atk and def values

--- Assets/Scripts/EnemyStat.cs
+++ Assets/Scripts/EnemyStat.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * to clarify def type vs attack type i've built myself this handy dandy chart Damage reduction
+ * 	++ = 50% dmg, + = 75% dmg, | = 100% dmg, - = 150% dmg, -- = 200% dmg
+ * 
+ * 		def:	light	|	medium	|	heavy	|	fortified
+ * atk		|			|			|			|
+ * normal	|	  +		|	  |		|	  -		|	  --
+ * piercing	|	  -		|	  +		|	  +		|	  -
+ * crushing	|	 --		|     /		|	  +		|	  ++
+ * heroic	|	  +		|	  +		|	  +		|	  |
+ * 
+ * 	i.e. if a normal attack of 20 hits a medium def of 5, the outcome is (20 - (5 * 1.00)) = 15 dmg
+ * if a piercing attack of 20 hits a heavy def of 5, the outcome is (20 - (5 * 0.75)) = 18.75, rounds down to 18 dmg
+ * 
+ */
+
 public class EnemyStat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
+	public int hp;	// amount of hp
+	public int def;	// type of defense (0 - light, 1 - medium, 2 - heavy, 3 - fortified)
+	public int defMagnitude;	// amount of def, determines damage reduction
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/TowerStat.cs
+++ Assets/Scripts/TowerStat.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class TowerStat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+	public int dmg;		// amount of damage sent to target
+	public int atk;		// type of damage (used for damage reduction calculation) (0 - normal, 1 - piercing, 2 - crushing, 3 - heroic)
+	public int range;	// how far the tower can attack, 1 point of range correlates to 1 grid pace of range (with most towers being 2x2)
+	public float spd;	// how fast the tower attacks, higher = faster, 1 = 1 atk ps, 2 = 2 atk ps, etc
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public int fx;		// what effect the tower might employ (requires further thought)
+	public float fxMagnitude; // how powerful/long the effect lasts
+
 }
