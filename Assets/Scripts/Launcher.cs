78487ad1c4796d79d35f180362422ba57c016d74
Evolved Bag <undercloudstudio@gmail.com>
2019-02-27 07:58:45 -0700

    Added Launcher
    connection to servers and such

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks{
+	
+	private byte maxPlayersPerRoom = 9;
+
+	void Awake(){PhotonNetwork.AutomaticallySyncScene = true;}
+		
+	public override void OnConnectedToMaster(){
+
+		PhotonNetwork.JoinRandomRoom();
+
+	}
+
+	public override void OnDisconnected(DisconnectCause cause){}
+
+	public override void OnJoinRandomFailed(short returnCode, string message){
+
+		PhotonNetwork.CreateRoom(null, new RoomOptions{MaxPlayer = maxPlayersPerRoom});
+
+	}
+
+	public override void OnJoinedRoom(){
+		
+
+
+	}
+
+}
