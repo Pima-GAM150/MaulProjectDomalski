b96b50562e7211d48510bdf390f50b4c68a01781
Evolved Bag <undercloudstudio@gmail.com>
2019-03-05 20:15:06 -0700

    Mapping

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+	public GameObject playerPrefab;
+	public List<Vector3> spawnLocations;
+
+    void Start(){
         
     }
-
-    // Update is called once per frame
+		
     void Update()
     {
         
