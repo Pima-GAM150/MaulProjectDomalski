0baedc28ca5473d5fff4918e620ed19ad32394ec
Sarah <undercloudstudio@gmail.com>
2019-03-06 14:05:54 -0700

    Implementation of building towers

--- Assets/Scripts/PlayerBuild.cs
+++ Assets/Scripts/PlayerBuild.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class PlayerBuild : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject BuildMenu, StandardMenu, cancelMenu, mouseInd;
+    public PlayerStat stats;
+    public List<GameObject> Towers;
+    public int selectedTower;
+    public bool isBuilding;
+
+    public void Build() { BuildMenu.SetActive(true); StandardMenu.SetActive(false); }
+    public void Cancel() { BuildMenu.SetActive(false); StandardMenu.SetActive(true); isBuilding = false; }
+    public void CancelIn() { BuildMenu.SetActive(true);  cancelMenu.SetActive(false); isBuilding = false; }
+    public void BuildX(int x) { BuildMenu.SetActive(false); cancelMenu.SetActive(true); isBuilding = true; selectedTower = x; }
+
+    private void Update()
     {
-        
+
+        if (isBuilding) {
+
+            mouseInd.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.Return)) {
+
+                GameObject temp = PhotonNetwork.Instantiate(Towers[selectedTower], mouseInd.transform.position, Quaternion.identity);
+                temp.GetComponent<SpriteRenderer>().color = stats.playerColor;
+                temp.GetComponent<TowerStat>().owner = stats;
+            
+            }
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
